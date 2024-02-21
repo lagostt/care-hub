104f188bed793ab294b522f5b77ba846ca9e31a3
saif <saifbahr54@gmail.com>
2024-02-21 23:03:00 +0100

    Add initial implementation of navbar

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,8 @@
+
+export default function NavBar(items:[]){
+    return(
+        <div className="flex flex-row px-2">
+            
+        </div>
+    )
+}
---
+++ src/components/navbar/navItems.ts
@@ -0,0 +1,15 @@
+
+export const landingPageItems = [
+   {
+        name: 'Home',
+        link: '/',
+    },
+    {
+        name: 'About',
+        link: '/about'
+    },
+    {
+        name: 'Services',
+        link: '/services'
+    }
+]
